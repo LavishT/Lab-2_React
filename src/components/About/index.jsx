8ccebef600ab962908a16783ee2990b48cc813bd
LavishT <lavishtalwar28@github.com>
2020-10-22 21:02:56 -0400

    Added About me

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,8 +4,9 @@ import Header from '../shared/Header';
 const Home = () => {
   return (
     <>
-      {/* You need to use your shared header component... */}
-      {/* You need a paragraph describing a fun fact about you */}
+    <Header title="Hey, Lavish here."/>
+    <Header title="I like to read non-fictional books."/>
+    <Header title="Currently, I am reading Marketing Management by Philip Kotler."/>
     </>
   );
 }
